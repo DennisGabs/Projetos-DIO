03d53d6f9a4e69dd483e55250c5d4b5a78084e0c
DennisGabriel <dennisgabr745@gmail.com>
2025-08-23 00:15:34 -0300

    componente dentro de componente

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Card } from './components/Card'
+import { Layout } from './components/Layout'
 
 function App() {
   return (
     <>
-      <Card id={1} title="Massa" description="Top demais bicho"/>
-
-      <Card id={2} title="Card do Dennis Lindo" description="Oloko bicho"/>
+      <Layout >
+        <Card id={1} title="Massa" description="Top demais bicho"/>
+        <Card id={2} title="Card do Dennis Lindo" description="Oloko bicho"/>
+      </Layout>
     </>
   )
 }
